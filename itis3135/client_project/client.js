e98e2ee64bb8fb2c463a173c67c85f20759049f7
cagej <cjenni26@uncc.edu>
2024-04-29 01:51:15 -0400

    client

--- itis3135/client_project/client.js
+++ itis3135/client_project/client.js
@@ -1,55 +1,68 @@
-function showHome() {
-    document.getElementById("home").style.display = "block";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("locations").style.display = "none";
-    document.getElementById("contact").style.display = "none";
-    document.getElementById("application").style.display = "none";
-    document.getElementById("routes").style.display = "none";
+const applyForm = document.getElementById('apply-form');
+const workContainer = document.getElementById("work-container");
+const addWorkBtn = document.getElementById("add-work-btn");
+const formContainer = document.getElementById('form-container');
+
+addWorkBtn.addEventListener('click', () => {
+    const card = document.createElement('div');
+    const jobTitleLabel = document.createElement('label');
+    const jobDescriptionLabel = document.createElement('label');
+    const jobTitle = document.createElement('input');
+    const jobDescription = document.createElement('textarea');
+    const divider = document.createElement('hr');
+    const deleteBtn = document.createElement('button');
+
+    jobTitleLabel.innerText = "Job Title ";
+    jobTitleLabel.appendChild(jobTitle);
+
+    jobDescriptionLabel.innerText = "\nJob Description ";
+    jobDescriptionLabel.appendChild(jobDescription);
+
+    jobTitle.setAttribute('type', 'text');
+    jobTitle.setAttribute('required', 'true')
+    jobDescription.setAttribute('type', 'text');
+    jobDescription.setAttribute('required', 'true')
+    
+    deleteBtn.textContent = "Delete";
+    deleteBtn.setAttribute('type', 'button');
+
+    deleteBtn.addEventListener('click', () => {
+        card.parentNode.removeChild(card);
+    });
+
+    card.append(jobTitleLabel);
+    card.append(jobDescriptionLabel);
+    card.append(deleteBtn);
+    card.append(divider);
+
+    workContainer.appendChild(card);
+});
+
+const jobExp = () => {
+    const HTMLString = 
+    `
+    <p>Thank you applying at Barone Sanitation! You will recieve a phone call within the next 7 business days regarding you application status.</p>
+    `; 
+
+    formContainer.innerHTML = HTMLString;
+};
+
+const priorWork = (inputJob) => {
+    if (inputJob.length === 0) {
+        return `<p>N/A</p>`;
+    } else {
+        let pos = 0;
+        let HTMLString = ``;
+        const max = inputJob.length / 2;
+        while (pos < max) {
+            HTMLString += `<li><b>${inputJob[pos * 2].value}:</b> ${inputJob[(pos * 2) + 1].value}</li>`;
+            pos++;
+        }
+        return HTMLString;
+    }
 }
 
-function showAbout() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "block";
-    document.getElementById("locations").style.display = "none";
-    document.getElementById("contact").style.display = "none";
-    document.getElementById("application").style.display = "none";
-    document.getElementById("routes").style.display = "none";
-}
-
-function showLocations() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("locations").style.display = "block";
-    document.getElementById("contact").style.display = "none";
-    document.getElementById("application").style.display = "none";
-    document.getElementById("routes").style.display = "none";
-}
-
-function showContact() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("locations").style.display = "none";
-    document.getElementById("contact").style.display = "block";
-    document.getElementById("application").style.display = "none";
-    document.getElementById("routes").style.display = "none";
-}
-
-function showApplication() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("locations").style.display = "none";
-    document.getElementById("contact").style.display = "none";
-    document.getElementById("application").style.display = "block";
-    document.getElementById("routes").style.display = "none";
-}
-
-function showRoutes() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("locations").style.display = "none";
-    document.getElementById("contact").style.display = "none";
-    document.getElementById("application").style.display = "none";
-    document.getElementById("routes").style.display = "block";
-}
-
-showHome();
+applyForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildIntroPage();
+});
